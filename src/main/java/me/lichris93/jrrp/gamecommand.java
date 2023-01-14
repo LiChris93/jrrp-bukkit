62216600f201e091ac4c3d24921943509f520b4f
LiChris93 <lihaouse@126.com>
2023-01-14 10:33:55 +0800

    improve logic,fix bug

--- src/main/java/me/lichris93/jrrp/gamecommand.java
+++ src/main/java/me/lichris93/jrrp/gamecommand.java
@@ -3,6 +3,7 @@ package me.lichris93.jrrp;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -11,122 +12,27 @@ import java.util.Set;
 
 public class gamecommand implements CommandExecutor {
     jrrp ins = jrrp.getself();
-
     @Override
-
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         if (commandSender.isOp()) {
-            if (args.length == 1 && args[0].equalsIgnoreCase("help")) {
-                commandSender.sendMessage("§a--------------[ jrrp ]--------------");
-                commandSender.sendMessage("§a/jrrp help              显示本帮助信息");
-                commandSender.sendMessage("§a/jrrp reload                 重载配置");
-                commandSender.sendMessage("§a/jrrp addadmin <qqid>          加管理");
-                commandSender.sendMessage("§a/jrrp deladmin <qqid>          减管理");
-                commandSender.sendMessage("§a/jrrp isadmin <qqid>    判断是否是管理");
-                commandSender.sendMessage("§a----------[ By LiChris93 ]-----------");
-                return true;
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("addadmin")) {
-                if (args[1].matches("[1-9][0-9]{4,14}")) {
-                    values.list.add(args[1]);
-                    final boolean sta = null != values.list;
-                    List<String> templist = new ArrayList<>();
-                    if (sta) {
-                        Set<String> set = new HashSet<>(values.list);
-                        templist.addAll(set);
-                    }
-                    StringBuilder temp = new StringBuilder();
-                    if (templist.size() > 1) {
-                        for (String s : templist) {
-                            temp.append(s).append(",");
-                        }
-                    } else {
-                        temp = new StringBuilder(templist.get(0));
-                    }
-                    values.config.set("admin", temp.toString());
-                    ins.saveConfig();
-                    commandSender.sendMessage("§a添加完成!" + args[1]);
-                    return true;
-                } else {
-                    commandSender.sendMessage("§c不正确的QQ号！");
-                    return true;
-                }
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("deladmin")) {
-                if (haspermission(Long.parseLong(args[1]))) {
-                    values.list.remove(args[1]);
-                    final boolean sta = null != values.list && values.list.size() > 0;//set转list用list方法
-                    List<String> templist = new ArrayList<>();
-                    if (sta) {
-                        Set<String> set = new HashSet<>(values.list);
-                        templist.addAll(set);
-                    }
-                    StringBuilder temp = new StringBuilder();
-                    if (templist.size() > 1) {
-                        for (String s : templist) {
-                            temp.append(s).append(",");
-                        }
-                    } else {
-                        temp = new StringBuilder(templist.get(0));
-                    }
-                    values.config.set("admin", temp.toString());
-                    ins.saveConfig();
-                    commandSender.sendMessage("§a移除完成!" + args[1]);
-                    return true;
-                } else if (!args[1].matches("[1-9][0-9]{4,14}")) {
-                    commandSender.sendMessage("§c不正确的QQ号！");
-                    return true;
-                } else {
-                    commandSender.sendMessage("§c该用户不是管理！");
-                    return true;
-                }
-            } else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-                try {
-                    values.list.clear();
-                    values.qqbot = values.config.getLong("bot");
-                    values.qqgroup = values.config.getLong("group");
-                    values.admin = values.config.getString("admin");
-                    values.version = values.config.getString("version");
-                    if (values.admin.contains(",")) {
-                        String[] temp = values.admin.split(",");
-                        for (String i : temp) {
-                            if (i.matches("[1-9][0-9]{4,14}")) {
-                                values.list.add(i);
-                            } else {
-                                ins.getLogger().warning(i + "不是有效的qq号");
-                            }
-                        }
-                    } else {
-                        if (values.admin.matches("[1-9][0-9]{4,14}")) {
-                            values.list.add(values.admin);
-                        } else {
-                            ins.getLogger().warning(values.admin + "不是有效的qq号");
-                        }
-                    }
-                    commandSender.sendMessage("§aconfig重载完成");
-                    return true;
-                } catch (Exception e) {
-                    ins.getLogger().info(e.toString());
-
-                    commandSender.sendMessage("§cconfig重载失败，详细信息查看控制台");
-                    return true;
-                }
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("isadmin")) {
-                if (haspermission(Long.parseLong(args[1]))) {
-                    commandSender.sendMessage("§a该用户是管理");
-                } else {
-                    commandSender.sendMessage("§c该用户不是管理");
-                }
-                return true;
+            if (args.length == 1 && args[0].equalsIgnoreCase("help")) {//jrrp help
+                showHelp(commandSender);
+            } else if (args.length == 2 && args[0].equalsIgnoreCase("addadmin")) {//jrrp addadmin <qqid>
+                addAdmin(commandSender,args[1]);
+            } else if (args.length == 2 && args[0].equalsIgnoreCase("deladmin")) {//jrrp deladmin <qqid>
+                delAdmin(commandSender,args[1]);
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {//jrrp reload
+                reloadConfigYml(commandSender);
+            } else if (args.length == 2 && args[0].equalsIgnoreCase("isadmin")) {//jrrp isadmin <qqid>
+               isAdmin(commandSender,args[1]);
             } else {
                 commandSender.sendMessage("§ajrrp v" + values.version + "正在这个服务器上运行, 使用 /jrrp help 来获取帮助");
-                return true;
             }
-
         } else {
             commandSender.sendMessage("§c你没有OP权限,无法执行命令");
-            return true;
         }
+        return true;
     }
-
     public boolean haspermission(long qqnum) {
         for (String s : values.list) {
             if (Long.toString(qqnum).equalsIgnoreCase(s)) {
@@ -135,5 +41,108 @@ public class gamecommand implements CommandExecutor {
         }
         return false;
 
+    }
+    public void showHelp(@NotNull CommandSender commandSender){
+        commandSender.sendMessage("§a--------------[ jrrp ]--------------");
+        commandSender.sendMessage("§a/jrrp help              显示本帮助信息");
+        commandSender.sendMessage("§a/jrrp reload                 重载配置");
+        commandSender.sendMessage("§a/jrrp addadmin <qqid>          加管理");
+        commandSender.sendMessage("§a/jrrp deladmin <qqid>          减管理");
+        commandSender.sendMessage("§a/jrrp isadmin <qqid>    判断是否是管理");
+        commandSender.sendMessage("§a----------[ By LiChris93 ]-----------");
+    }
+    public void addAdmin(CommandSender commandSender, @NotNull String qqnum){
+        if (qqnum.matches("[1-9][0-9]{4,14}")) {
+            values.list.add(qqnum);
+            final boolean sta = null != values.list;
+            List<String> templist = new ArrayList<>();
+            if (sta) {
+                Set<String> set = new HashSet<>(values.list);
+                templist.addAll(set);
+            }
+            StringBuilder temp = new StringBuilder();
+            if (templist.size() > 1) {
+                for (String s : templist) {
+                    temp.append(s).append(",");
+                }
+            } else {
+                temp = new StringBuilder(templist.get(0));
+            }
+            values.config.set("admin", temp.toString());
+            ins.saveConfigYml();
+            commandSender.sendMessage("§a添加完成!" + qqnum);
+        } else {
+            commandSender.sendMessage("§c不正确的QQ号！");
+        }
+    }
+    public void reloadConfigYml(CommandSender commandSender){
+        ins.reloadConfig();
+        values.config = ins.getConfig();
+        try {
+            values.list.clear();
+            values.qqbot = values.config.getLong("bot");
+            values.qqgroup = values.config.getLong("group");
+            values.admin = values.config.getString("admin");
+            values.jrrpmes = values.config.getString("lang.jrrpmes");
+            values.version = values.config.getString("version");
+            values.jrrpclear = values.config.getString("lang.jrrpclear");
+            values.sendmap = values.config.getString("lang.sendmap");
+            values.getfailmes = values.config.getString("lang.getfailmes");
+            values.getsucceedmes = values.config.getString("lang.getsucceedmes");
+            if (values.admin.contains(",")) {
+                String[] temp = values.admin.split(",");
+                for (String i : temp) {
+                    if (i.matches("[1-9][0-9]{4,14}")) {
+                        values.list.add(i);
+                    } else {
+                        ins.warn(i + "不是有效的qq号");
+                    }
+                }
+            } else {
+                if (values.admin.matches("[1-9][0-9]{4,14}")) {
+                    values.list.add(values.admin);
+                } else {
+                    ins.warn(values.admin + "不是有效的qq号");
+                }
+            }
+            commandSender.sendMessage("§aconfig重载完成");
+        } catch (Exception e) {
+            ins.info(e.toString());
+
+            commandSender.sendMessage("§cconfig重载失败，详细信息查看控制台");
+        }
+    }
+    public void isAdmin(CommandSender commandSender,String qqnum){
+        if (haspermission(Long.parseLong(qqnum))) {
+            commandSender.sendMessage("§a该用户是管理");
+        } else {
+            commandSender.sendMessage("§c该用户不是管理");
+        }
+    }
+    public void delAdmin(CommandSender commandSender,String qqnum){
+        if (haspermission(Long.parseLong(qqnum))) {
+            values.list.remove(qqnum);
+            final boolean sta = null != values.list && values.list.size() > 0;//set转list用list方法
+            List<String> templist = new ArrayList<>();
+            if (sta) {
+                Set<String> set = new HashSet<>(values.list);
+                templist.addAll(set);
+            }
+            StringBuilder temp = new StringBuilder();
+            if (templist.size() > 1) {
+                for (String s : templist) {
+                    temp.append(s).append(",");
+                }
+            } else {
+                temp = new StringBuilder(templist.get(0));
+            }
+            values.config.set("admin", temp.toString());
+            ins.saveConfigYml();
+            commandSender.sendMessage("§a移除完成!" + qqnum);
+        } else if (!qqnum.matches("[1-9][0-9]{4,14}")) {
+            commandSender.sendMessage("§c不正确的QQ号！");
+        } else {
+            commandSender.sendMessage("§c该用户不是管理！");
+        }
     }
 }
--- src/main/java/me/lichris93/jrrp/jrrp.java
+++ src/main/java/me/lichris93/jrrp/jrrp.java
@@ -7,61 +7,100 @@ import java.util.*;
 
 
 public class jrrp extends JavaPlugin {
-    long loadtime;
 
     @Override
     public void onEnable() {
+        //Make the values not null
         values.config = this.getConfig();
         values.instance = this;
+        //Create default yml file when missing
         saveDefaultConfig();
-        loadtime = System.currentTimeMillis();
-        getLogger().info("-------------------------------------");
-        getLogger().info(" L      IIIII   999999999  333333333 ");
-        getLogger().info(" L        I     9       9          3 ");
-        getLogger().info(" L        I     9       9          3 ");
-        getLogger().info(" L        I     999999999  333333333 ");
-        getLogger().info(" L        I             9          3 ");
-        getLogger().info(" L        I             9          3");
-        getLogger().info(" L        I     9       9          3");
-        getLogger().info(" LLLLLL IIIII   999999999  333333333 ");
-        getLogger().info("-------------------------------------");
-        Bukkit.getPluginManager().registerEvents(new groupmsg(), this);
-        getLogger().info("事件监听器注册完成");
-        Bukkit.getPluginCommand("jrrp").setExecutor(new gamecommand());
-        getLogger().info("命令执行器注册完成");
-        values.qqbot = values.config.getLong("bot");
-        values.qqgroup = values.config.getLong("group");
-        values.admin = values.config.getString("admin");
-        values.jrrpmes = values.config.getString("lang.jrrpmes");
-        values.version = values.config.getString("version");
-        values.jrrpclear = values.config.getString("lang.jrrpclear");
-        values.sendmap = values.config.getString("lang.sendmap");
-        values.getfailmes = values.config.getString("lang.getfailmes");
-        values.getsucceedmes = values.config.getString("lang.getsucceedmes");
-        if (values.admin.contains(",")) {
-            String[] temp = values.admin.split(",");
-            values.list.addAll(Arrays.asList(temp));
-        } else {
-            values.list.add(values.admin);
-        }
-        getLogger().info("config读取完成");
-        getLogger().info("jrrp 加载完成！——By LiChris93[" + (System.currentTimeMillis() - loadtime) + "ms]");
+        //Begin Enabling
+        //Record how much time does enable this plugin use
+        long loadtime = System.currentTimeMillis();
+        info("jrrp is now enabling ——By LiChris93");
+        //Register bot event
+        regEvent();
+        //Register game command
+        regCommand();
+        //Load config.yml
+        loadConfig();
+        //Finish Enabling
+        info("jrrp 加载完成！——By LiChris93[" + (System.currentTimeMillis() - loadtime) + "ms]");
 
     }
-
     @Override
     public void onDisable() {
-        saveConfig();
-        getLogger().info("config已保存");
-        getLogger().info("jrrp 卸载完毕！——By LiChris93");
+        //Save config.yml when disabling
+        saveConfigYml();
+        //Finish Disabling
+        info("jrrp 卸载完毕！——By LiChris93");
     }
-
     public static jrrp getself() {
         return values.instance;
     }
-
-    public static void main(String[] args) {
-
+    public void regCommand(){
+        try {
+            Bukkit.getPluginCommand("jrrp").setExecutor(new gamecommand());
+            info("命令执行器注册完成");
+        }catch (Exception e){
+            warn("jrrp加载失败！原因：命令执行器注册失败，详情请看控制台");
+            e.printStackTrace();
+            //Disable plugin
+            this.getPluginLoader().disablePlugin(this);
+        }
+    }
+    public void regEvent(){
+        try {
+            Bukkit.getPluginManager().registerEvents(new groupmsg(), this);
+            info("事件监听器注册完成");
+        }catch (Exception e){
+            warn("jrrp加载失败！原因：事件监听器注册失败，详情请看控制台");
+            e.printStackTrace();
+            //Disable plugin
+            this.getPluginLoader().disablePlugin(this);
+        }
+    }
+    public void loadConfig() {
+        try {
+            values.qqbot = values.config.getLong("bot");
+            values.qqgroup = values.config.getLong("group");
+            values.admin = values.config.getString("admin");
+            values.jrrpmes = values.config.getString("lang.jrrpmes");
+            values.version = values.config.getString("version");
+            values.jrrpclear = values.config.getString("lang.jrrpclear");
+            values.sendmap = values.config.getString("lang.sendmap");
+            values.getfailmes = values.config.getString("lang.getfailmes");
+            values.getsucceedmes = values.config.getString("lang.getsucceedmes");
+            if (values.admin.contains(",")) {
+                String[] temp = values.admin.split(",");
+                values.list.addAll(Arrays.asList(temp));
+            } else {
+                values.list.add(values.admin);
+            }
+            info("config读取完成");
+        }catch (Exception e){
+            warn("jrrp加载失败！原因：config读取失败，详情请看控制台");
+            e.printStackTrace();
+            //Disable plugin
+            this.getPluginLoader().disablePlugin(this);
+        }
 
     }
+    public void saveConfigYml(){
+        try {
+            saveConfig();
+            info("config已保存");
+        }catch (Exception e){
+            warn("config保存失败，详情请看控制台");
+            e.printStackTrace();
+        }
+    }
+    public void info(String text){
+        getLogger().info(text);
+    }
+    public void warn(String text){
+        getLogger().warning(text);
+    }
+
 }
